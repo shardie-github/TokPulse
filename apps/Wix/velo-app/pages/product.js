8a2b9b9649a91283342dcdb4125ac3e664fbece8
Scott Hardie <scottrmhardie@gmail.com>
2025-09-20 11:39:16 -0400

    Create product.js

---
+++ apps/Wix/velo-app/pages/product.js
@@ -0,0 +1,6 @@
+import { initPixels } from '../analytics/pixels.js';
+
+$w.onReady(function () {
+  // Example: initialize with site settings or Wix Secrets
+  initPixels({ tiktokPixelId: '', metaPixelId: '', ga4Id: '' });
+});
